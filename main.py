0deea80e33a0af334fdd5fb126e7953084f6b8bd
nickjohnsondls <nickjj2005@gmail.com>
2023-12-22 03:05:29 -0800

    meh

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from flask import Flask, render_template
 from app import extraction, container
 from sumry import summarize_text_list
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# do the app 
+# INitialize Flask App  
 app = Flask(__name__)
 
-#Biotech extraction
-titlesBio = extraction('https://www.biospace.com/news/money/')
-#summaryBiotech = summarize_text_list(titlesBio)
+# Initialize Scheduler
+scheduler = BackgroundScheduler()
 
-#Energy Extraction
-titlesEnergy = extraction('https://seekingalpha.com/market-news/energy')
+# Global variable to store the results
+biotech = ''
+energy = ''
+overall = ''
 
-#yfinance extraction 
-titlesOverall = extraction('https://finance.yahoo.com/tech/')
+
+def scrape_and_summarize():
+
+    #Biotech extraction
+    titlesBio = extraction('https://www.biospace.com/news/money/')
+    biotech = titlesBio
+   
+
+    #Energy Extraction
+    titlesEnergy = extraction('https://seekingalpha.com/market-news/energy')
+    energy = titlesEnergy
+
+    #yfinance extraction 
+    titlesOverall = extraction('https://finance.yahoo.com/tech/')
+    overall = titlesOverall
+
+
+# Add a scheduled job
+scheduler.add_job(func=scrape_and_summarize, trigger='interval', hours=1)  # Adjust the interval as needed
+scheduler.start()
+
 
 @app.route('/')
 def index():
-    return render_template('template.html', summary =titlesBio, energysum=titlesEnergy, Techsummary=titlesOverall )  # Your HTML file
+    return render_template('template.html', summary =biotech, energysum=energy, Techsummary=overall )  # Your HTML file
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
